91a8130f47699ee3543b95e318c08fdc9940fb66
maddySeneca <mrajpal4@myseneca.ca>
2025-02-19 12:40:39 -0500

    Front end files and test component added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TestComponent from "./components/testComponent";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
